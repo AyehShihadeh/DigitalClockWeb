7ff86ef492a4ae4b7c2ef661c5fe8070168e2c1b
Ayeh Shihadeh <AyehShihadeh0@gmail.com>
2023-02-23 15:53:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const setTheme = (theme) => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+    }
+
+document.getElementById('theme-select').addEventListener('change', function() {
+    setTheme(this.value);
+});
+
+const getTheme = () => {
+    const theme = localStorage.getItem('theme');
+    theme && setTheme(theme);
+}
+    getTheme();
+
 function currentTime() {
     let date = new Date();
     let hour = date.getHours();
